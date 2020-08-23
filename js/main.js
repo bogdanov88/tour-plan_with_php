cd40376e96c2565d5b2a4ef070d904909a970cca
bogdanov88 <a09061988@gmail.com>
2020-08-24 00:55:43 +0300

    create: modal window block and responsive

--- js/main.js
+++ js/main.js
@@ -1,47 +1,70 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
 
-  //переключение стрелками клавиатуры
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+    //переключение стрелками клавиатуры
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
 
-  effect: "coverflow",
-});
-// для того чтобы существовало 2 разных слайдера на странице = их необходимо инициализировать ОТДЕЛЬНО !!!
-// СЕЙЧАС - ОНИ иницализируются по предустановленному классу .swiper-container в строке new Swiper('.swiper-container', {
-// это надо исправить!
-// дублируем, меняем названия переменных, и имя в параметрах.
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
+    effect: "coverflow",
+  });
+  // для того чтобы существовало 2 разных слайдера на странице = их необходимо инициализировать ОТДЕЛЬНО !!!
+  // СЕЙЧАС - ОНИ иницализируются по предустановленному классу .swiper-container в строке new Swiper('.swiper-container', {
+  // это надо исправить!
+  // дублируем, меняем названия переменных, и имя в параметрах.
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
 
-  //переключение стрелками клавиатуры
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    //переключение стрелками клавиатуры
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("клик по кнопке меню");
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("клик по кнопке меню");
 
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom_visible");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom_visible");
+  });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay_visible');
+    modalDialog.addClass('modal__dialog_visible');
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay_visible');
+    modalDialog.removeClass('modal__dialog_visible');
+  }
+
 });
